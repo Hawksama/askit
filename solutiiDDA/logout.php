52191163da301eb60c5a1dcba5c1fe06e1eec48c
Carabus Manuel Alexandru <manue971@icloud.com>
2020-03-19 15:14:30 +0200

    remove unused folder

--- solutiiDDA/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-setcookie("user", "", time()-3600);
-setcookie("username", "", time()-3600);
-echo'<script type="text/javascript">window.location.replace("/solutiiDDA/login.php")</script>';
-?>
